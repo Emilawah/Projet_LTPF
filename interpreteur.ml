e5849277a9506d59caf2f77ec5227fb3bd2d98f1
Emilio <emistien@gmail.com>
2025-12-08 19:21:45 +0100

    q3.4.2 & 3.4.3 done

--- interpreteur.ml
+++ interpreteur.ml
@@ -38,11 +38,45 @@ let rec eval_expr_sos (e : wexpr) (s : Etat.t) : int =
       let v1 = eval_expr_sos e1 s in
       let v2 = eval_expr_sos e2 s in
       if v1 = 1 && v2 = 1 then 1 else 0
-  | ENot e0 -> 
-      if eval_expr_sos e0 s = 1 then 0 else 1
+  | ENot e -> 
+      if eval_expr_sos e s = 1 then 0 else 1
 
 let exec_assign (v : var) (e : wexpr) (s : Etat.t) : Etat.t =
   let val_e = eval_expr_sos e s in
   Etat.update s v val_e
 
 
+
+
+(* 3.4.2 *)
+
+type config = 
+|Inter of winstr * Etat.t 
+|Final of Etat.t
+
+
+let rec faire_un_pas (prog: winstr) (s: Etat.t) : config = 
+  match prog with
+  |Skip -> Final s
+  |Assign (v,e) -> Final (exec_assign v e s)
+  |Seq(i1,i2) -> (match faire_un_pas i1 s with
+                  |Final s1 -> Inter(i2,s1)
+                  |Inter(i1',s1) -> Inter (Seq(i1',i2),s1))
+                  
+  |If(v,i1,i2) -> let cond = Etat.get s v in 
+                    if cond = 1 then Inter(i1,s) else Inter(i2,s)
+  
+  |While(v,i) -> Inter(If(v,Seq(i,While(v,i)),Skip),s);;
+
+
+  
+(* 3.4.3 *)
+
+let rec executer (prog:winstr) (s: Etat.t) : Etat.t = 
+  match faire_un_pas prog s with
+  |Final s -> s
+  |Inter(i,s) -> executer i s;;
+
+
+
+(* Tests*)
